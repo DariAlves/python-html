b7e2a257274b22a835af0b506df665fe95179b18
Dari Alves <darieltondf@gmail.com>
2021-03-20 20:49:01 -0300

    templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Dari'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Snyder Cut movie was so cool!'
+        },
+        {
+            'author': {'username': 'DC'},
+            'body': 'UFC sets Chandler vs. Oliveira fight for lightweight title'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
